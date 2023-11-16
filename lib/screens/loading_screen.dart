03f022db02db1a78255a6e8672d395920305cabc
itzmars <marsdoulens@proton.me>
2023-11-16 14:33:37 -0800

    add http packages

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:clima_cast/services/location.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -13,16 +14,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void initState() {
     super.initState();
     getLocation();
+    print("Get Data");
+    getData();
   }
 
-  Future<void> getLocation() async {
+  void getLocation() async {
     Location location = Location();
-
     await location.getCurrentLocalisation();
+  }
+
+  void getData() async {
+    var response = await http.get(Uri.parse(
+        'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=1d8421344d08bdf8206821b040d5de7e'));
+    print(response.body);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Center(child: Text("Hello, world")),
+    );
   }
 }
